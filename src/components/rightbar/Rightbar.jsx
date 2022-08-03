efb97d53e7e3ee79025a4e534c3c5470c477b04c
TeodorTodorovT <teodortodorov752@gmail.com>
2022-08-03 11:12:21 +0300

    Make online friends dynamic

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,6 @@
 import "./rightbar.css"
+import { Users } from "../../dummyData"
+import Online from "../online/Online"
 
 const Rightbar = () =>{
     return(
@@ -13,55 +15,9 @@ const Rightbar = () =>{
                 <img src="assets/ad.jpg" alt="" className="rightbarAd" />
                 <h4 className="rightbarTitle">Online Friend</h4>
                     <ul className="rightbarFriendList">
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
-                        <li className="rightbarFriend">
-                            <div className="rightbarProfileImgContainer">
-                                <img src="assets/person/4.jpg" alt="" className="rightbarProfileImg" />
-                                <span className="rightbarOnline"></span>
-                            </div>
-                            <span className="rightbarUserName">Will Smith</span>
-                        </li>
+                        {Users.map(u => (
+                            <Online key={u.id} user={u}/>
+                        ))}
                     </ul>
             </div>
         </div>
