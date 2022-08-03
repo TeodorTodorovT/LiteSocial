392dc95c1ad90abc829c1ef78b1fe0eb44d317d4
TeodorTodorovT <teodortodorov752@gmail.com>
2022-08-03 10:56:45 +0300

    add some static data

---
+++ src/dummyData.js
@@ -0,0 +1,72 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "assets/person/1.jpg",
+      username: "Dwayne Johnson",
+    },
+    {
+      id:2,
+      profilePicture: "assets/person/2.jpg",
+      username: "Bill Gates",
+    },
+    {
+      id:3,
+      profilePicture: "assets/person/3.jpg",
+      username: "Elon Musk",
+    },
+    {
+      id:4,
+      profilePicture: "assets/person/4.jpg",
+      username: "Will Smith",
+    },
+    {
+      id:5,
+      profilePicture: "assets/person/5.jpg",
+      username: "Jeff Bezos",
+    },
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "waterfall",
+      photo: "assets/post/1.jpg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "assets/post/2.jpg",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "River",
+      photo: "assets/post/3.jpg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: "assets/post/4.jpg",
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    },
+    {
+      id: 5,
+      photo: "assets/post/5.jpg",
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+  ];
