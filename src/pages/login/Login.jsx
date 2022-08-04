d2bf8f967bc1220979c2dad09af520da045f0bef
TeodorTodorovT <teodortodorov752@gmail.com>
2022-08-04 15:07:31 +0300

    more login additions

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,15 @@
+import { useRef } from "react";
 import "./login.css"
 
 export default function Login() {
+
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(email.current.value);
+    }
   return (
     <div className="login">
         <div className="loginWrapper">
@@ -9,13 +18,20 @@ export default function Login() {
                 <span className="loginDesc">Connect with your friends!</span>
             </div>
             <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Email" type="email" className="loginInput" />
-                    <input placeholder="Password" type="password" className="loginInput" />
+                <form className="loginBox" onSubmit={handleClick}>
+                    <input placeholder="Email" type="email" className="loginInput" ref={email} required/>
+                    <input 
+                        placeholder="Password" 
+                        type="password" 
+                        className="loginInput" 
+                        ref={password} 
+                        required 
+                        minLength="6"
+                    />
                     <button className="loginButton">Log in</button>
                     <span className="loginForgot">Forgot password?</span>
                     <button className="loginRegisterButton">Create a new account</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
